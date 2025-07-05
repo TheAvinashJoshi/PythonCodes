5e763519050a40760f5a66aaabd2e51c17e1df4f
Avinash Joshi <ajoshi0100@gmail.com>
2025-07-05 15:02:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+# simple statement
+print("Hello World \n")
+
+# print something 10 times vertical with \n
+print("Hello World \n" * 10)
+
+# print something 10 times horizontal
+print("Hello World " * 10)
+
